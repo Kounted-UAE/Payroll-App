4a98798d2aff5bf67babef679204945463a78815
Advontier <eben.johansen@gmail.com>
2025-08-03 13:18:08 +0400

    not naving it

--- components/dashboard-layout/DashboardLayout.tsx
+++ components/dashboard-layout/DashboardLayout.tsx
@@ -9,16 +9,21 @@ import { TopNavbar } from "./TopNavbar";
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider>
-      <div className="flex min-h-screen w-full">
-        {/* Sidebar */}
-        <div className="hidden md:block">
-          <AppSidebar />
-        </div>
+      <div className="flex min-h-screen w-full flex-col">
+        {/* Top Navbar - Full width across the top */}
+        <TopNavbar />
+        
+        {/* Main content area with sidebar and page content */}
+        <div className="flex flex-1">
+          {/* Sidebar - Starts below navbar */}
+          <div className="hidden md:block">
+            <AppSidebar />
+          </div>
 
-        {/* Content Area - Use CSS custom properties for dynamic width */}
-        <div className="flex flex-1 flex-col transition-[margin-left] duration-200 ease-linear md:ml-[var(--sidebar-width,16rem)]">
-          <TopNavbar />
-          <main className="flex-1 p-2">{children}</main>
+          {/* Page content - To the right of sidebar */}
+          <main className="flex-1 p-2 transition-[margin-left] duration-200 ease-linear md:ml-[var(--sidebar-width,16rem)]">
+            {children}
+          </main>
         </div>
       </div>
     </SidebarProvider>
